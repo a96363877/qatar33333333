ff1dc334ecd6865765100ed1af77b8112bc38013
a96363877 <a96363877@gmail.com>
2025-02-04 12:41:30 -0800

    BDV

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -17,13 +17,13 @@ import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/input-otp
 
 const firebaseConfig = {
   // Your Firebase configuration object goes here
-  apiKey: "AIzaSyDNF8IclQF8rBFiq8yPA-4Bczxzx2IJSoY",
-  authDomain: "atar1-f0830.firebaseapp.com",
-  projectId: "atar1-f0830",
-  storageBucket: "atar1-f0830.firebasestorage.app",
-  messagingSenderId: "973639712303",
-  appId: "1:973639712303:web:2f32be1d28949da178d0da",
-  measurementId: "G-M9JLXES2TP"
+  apiKey: "AIzaSyCEK4irWtP1f6y_J1Gj3kNwsyLdItndt5Y",
+  authDomain: "sdkk-22bbc.firebaseapp.com",
+  projectId: "sdkk-22bbc",
+  storageBucket: "sdkk-22bbc.firebasestorage.app",
+  messagingSenderId: "1039314059862",
+  appId: "1:1039314059862:web:e14e764f77ce86c817b1f7",
+  measurementId: "G-K0L3MMPDR7"
 }
 
 const app = initializeApp(firebaseConfig)
